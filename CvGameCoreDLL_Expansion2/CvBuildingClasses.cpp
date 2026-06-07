dinwkey/Community-Patch-DLL

commit e7680355b41ffadaa94a286d9afb09bf34e11c6b

CS Friend and Ally tables now x100 for future balancing (#12442)
All related methods and variables updated to times100 version.
Existing buildings are updated.

CvGameCoreDLL_Expansion2/CvBuildingClasses.cpp
@@ -388,8 +388,8 @@ CvBuildingEntry::CvBuildingEntry(void):
 	m_pbBuildingClassNeededNowhere(NULL),
 	m_piNumSpecFreeUnits(NULL),
 	m_ppiResourcePlotsToPlace(),
-	m_piYieldPerFriend(NULL),
-	m_piYieldPerAlly(NULL),
+	m_piYieldPerFriendTimes100(NULL),
+	m_piYieldPerAllyTimes100(NULL),
 	m_piYieldChangeWorldWonder(NULL),
 	m_piYieldChangeWorldWonderGlobal(NULL),
 	m_piLuxuryYieldChanges(NULL),
@@ -542,8 +542,8 @@ CvBuildingEntry::~CvBuildingEntry(void)
 	SAFE_DELETE_ARRAY(m_piNumSpecFreeUnits);
 	m_ppiResourcePlotsToPlace.clear();
 	SAFE_DELETE_ARRAY(m_paiResourceHappinessChange);
-	SAFE_DELETE_ARRAY(m_piYieldPerFriend);
-	SAFE_DELETE_ARRAY(m_piYieldPerAlly);
+	SAFE_DELETE_ARRAY(m_piYieldPerFriendTimes100);
+	SAFE_DELETE_ARRAY(m_piYieldPerAllyTimes100);
 	SAFE_DELETE_ARRAY(m_piYieldChangeWorldWonder);
 	SAFE_DELETE_ARRAY(m_piYieldChangeWorldWonderGlobal);
 	SAFE_DELETE_ARRAY(m_piLuxuryYieldChanges);
@@ -1094,8 +1094,8 @@ bool CvBuildingEntry::CacheResults(Database::Results& kResults, CvDatabaseUtilit
 	kUtility.SetYields(m_piYieldChangeFromPassingTR, "Building_YieldChangesFromPassingTR", "BuildingType", szBuildingType);
 	kUtility.SetYields(m_piYieldPerFranchise, "Building_YieldPerFranchise", "BuildingType", szBuildingType);
 
-	kUtility.SetYields(m_piYieldPerFriend, "Building_YieldPerFriend", "BuildingType", szBuildingType);
-	kUtility.SetYields(m_piYieldPerAlly, "Building_YieldPerAlly", "BuildingType", szBuildingType);
+	kUtility.SetYields(m_piYieldPerFriendTimes100, "Building_YieldPerFriendTimes100", "BuildingType", szBuildingType);
+	kUtility.SetYields(m_piYieldPerAllyTimes100, "Building_YieldPerAllyTimes100", "BuildingType", szBuildingType);
 
 	kUtility.SetYields(m_piYieldChangeWorldWonder, "Building_YieldChangeWorldWonder", "BuildingType", szBuildingType);
 	kUtility.SetYields(m_piYieldChangeWorldWonderGlobal, "Building_YieldChangeWorldWonderGlobal", "BuildingType", szBuildingType);
@@ -4516,18 +4516,18 @@ bool CvBuildingEntry::IsResourcePlotsToPlace() const
 {
 	return !m_ppiResourcePlotsToPlace.empty();
 }
-int CvBuildingEntry::GetYieldPerFriend(int i) const
+int CvBuildingEntry::GetYieldPerFriendTimes100(int i) const
 {
 	PRECONDITION(i < NUM_YIELD_TYPES, "Index out of bounds");
 	PRECONDITION(i > -1, "Index out of bounds");
-	return m_piYieldPerFriend ? m_piYieldPerFriend[i] : -1;
+	return m_piYieldPerFriendTimes100 ? m_piYieldPerFriendTimes100[i] : -1;
 }
 
-int CvBuildingEntry::GetYieldPerAlly(int i) const
+int CvBuildingEntry::GetYieldPerAllyTimes100(int i) const
 {
 	PRECONDITION(i < NUM_YIELD_TYPES, "Index out of bounds");
 	PRECONDITION(i > -1, "Index out of bounds");
-	return m_piYieldPerAlly ? m_piYieldPerAlly[i] : -1;
+	return m_piYieldPerAllyTimes100 ? m_piYieldPerAllyTimes100[i] : -1;
 }
 
 int CvBuildingEntry::GetYieldChangeWorldWonder(int i) const

CvGameCoreDLL_Expansion2/CvCity.cpp
@@ -418,8 +418,8 @@ CvCity::CvCity() :
 	, m_aiYieldFromUnitLevelUp()
 	, m_aiYieldFromUnitLevelUpGlobal()
 	, m_aiYieldFromCombatExperienceTimes100()
-	, m_aiYieldPerAlly()
-	, m_aiYieldPerFriend()
+	, m_aiYieldPerAllyTimes100()
+	, m_aiYieldPerFriendTimes100()
 	, m_aiYieldFromInternalTREnd()
 	, m_aiYieldFromInternationalTREnd()
 	, m_aiYieldFromInternalTR()
@@ -1346,8 +1346,8 @@ void CvCity::reset(int iID, PlayerTypes eOwner, int iX, int iY, bool bConstructo
 	m_aiYieldFromUnitLevelUp.resize(NUM_YIELD_TYPES);
 	m_aiYieldFromUnitLevelUpGlobal.resize(NUM_YIELD_TYPES);
 	m_aiYieldFromCombatExperienceTimes100.resize(NUM_YIELD_TYPES);
-	m_aiYieldPerAlly.resize(NUM_YIELD_TYPES);
-	m_aiYieldPerFriend.resize(NUM_YIELD_TYPES);
+	m_aiYieldPerAllyTimes100.resize(NUM_YIELD_TYPES);
+	m_aiYieldPerFriendTimes100.resize(NUM_YIELD_TYPES);
 	m_aiYieldFromInternalTREnd.resize(NUM_YIELD_TYPES);
 	m_aiYieldFromInternationalTREnd.resize(NUM_YIELD_TYPES);
 	m_aiYieldFromInternalTR.resize(NUM_YIELD_TYPES);
@@ -1457,8 +1457,8 @@ void CvCity::reset(int iID, PlayerTypes eOwner, int iX, int iY, bool bConstructo
 		m_aiYieldFromUnitLevelUp[iI] = 0;
 		m_aiYieldFromUnitLevelUpGlobal[iI] = 0;
 		m_aiYieldFromCombatExperienceTimes100[iI] = 0;
-		m_aiYieldPerAlly[iI] = 0;
-		m_aiYieldPerFriend[iI] = 0;
+		m_aiYieldPerAllyTimes100[iI] = 0;
+		m_aiYieldPerFriendTimes100[iI] = 0;
 		m_aiYieldFromInternalTREnd[iI] = 0;
 		m_aiYieldFromInternationalTREnd[iI] = 0;
 		m_aiYieldFromInternalTR[iI] = 0;
@@ -14741,8 +14741,8 @@ void CvCity::processBuilding(BuildingTypes eBuilding, int iChange, bool bFirst, 
 				ChangeYieldFromCombatExperienceTimes100(eYield, pBuildingInfo->GetYieldFromCombatExperienceTimes100(eYield) * iChange);
 			}
 
-			ChangeYieldPerAlly(eYield, pBuildingInfo->GetYieldPerAlly(eYield) * iChange);
-			ChangeYieldPerFriend(eYield, pBuildingInfo->GetYieldPerFriend(eYield) * iChange);
+			ChangeYieldPerAllyTimes100(eYield, pBuildingInfo->GetYieldPerAllyTimes100(eYield) * iChange);
+			ChangeYieldPerFriendTimes100(eYield, pBuildingInfo->GetYieldPerFriendTimes100(eYield) * iChange);
 
 			if (GetEventBuildingClassCityYield(eBuildingClass, eYield) != 0)
 			{
@@ -24665,17 +24665,17 @@ void CvCity::ChangeYieldFromCombatExperienceTimes100(YieldTypes eIndex, int iCha
 
 //	--------------------------------------------------------------------------------
 /// Extra yield from building
-int CvCity::GetYieldPerAlly(YieldTypes eIndex) const
+int CvCity::GetYieldPerAllyTimes100(YieldTypes eIndex) const
 {
 	VALIDATE_OBJECT();
 	PRECONDITION(eIndex >= 0, "eIndex expected to be >= 0");
 	PRECONDITION(eIndex < NUM_YIELD_TYPES, "eIndex expected to be < NUM_YIELD_TYPES");
-	return m_aiYieldPerAlly[eIndex];
+	return m_aiYieldPerAllyTimes100[eIndex];
 }
 
 //	--------------------------------------------------------------------------------
 /// Extra yield from building
-void CvCity::ChangeYieldPerAlly(YieldTypes eIndex, int iChange)
+void CvCity::ChangeYieldPerAllyTimes100(YieldTypes eIndex, int iChange)
 {
 	VALIDATE_OBJECT();
 	PRECONDITION(eIndex >= 0, "eIndex expected to be >= 0");
@@ -24683,23 +24683,23 @@ void CvCity::ChangeYieldPerAlly(YieldTypes eIndex, int iChange)
 
 	if (iChange != 0)
 	{
-		m_aiYieldPerAlly[eIndex] = m_aiYieldPerAlly[eIndex] + iChange;
-		ASSERT(GetYieldPerAlly(eIndex) >= 0);
+		m_aiYieldPerAllyTimes100[eIndex] = m_aiYieldPerAllyTimes100[eIndex] + iChange;
+		ASSERT(GetYieldPerAllyTimes100(eIndex) >= 0);
 	}
 }
 //	--------------------------------------------------------------------------------
 /// Extra yield from building
-int CvCity::GetYieldPerFriend(YieldTypes eIndex) const
+int CvCity::GetYieldPerFriendTimes100(YieldTypes eIndex) const
 {
 	VALIDATE_OBJECT();
 	PRECONDITION(eIndex >= 0, "eIndex expected to be >= 0");
 	PRECONDITION(eIndex < NUM_YIELD_TYPES, "eIndex expected to be < NUM_YIELD_TYPES");
-	return m_aiYieldPerFriend[eIndex];
+	return m_aiYieldPerFriendTimes100[eIndex];
 }
 
 //	--------------------------------------------------------------------------------
 /// Extra yield from building
-void CvCity::ChangeYieldPerFriend(YieldTypes eIndex, int iChange)
+void CvCity::ChangeYieldPerFriendTimes100(YieldTypes eIndex, int iChange)
 {
 	VALIDATE_OBJECT();
 	PRECONDITION(eIndex >= 0, "eIndex expected to be >= 0");
@@ -24707,8 +24707,8 @@ void CvCity::ChangeYieldPerFriend(YieldTypes eIndex, int iChange)
 
 	if (iChange != 0)
 	{
-		m_aiYieldPerFriend[eIndex] = m_aiYieldPerFriend[eIndex] + iChange;
-		ASSERT(GetYieldPerFriend(eIndex) >= 0);
+		m_aiYieldPerFriendTimes100[eIndex] = m_aiYieldPerFriendTimes100[eIndex] + iChange;
+		ASSERT(GetYieldPerFriendTimes100(eIndex) >= 0);
 	}
 }
 
@@ -25452,7 +25452,7 @@ int CvCity::GetEffectiveYieldRateFromCSAllianceTimes100(YieldTypes eIndex) const
 
 	int iValue = GetBaseYieldRateFromCSAlliance(eIndex) * (100 + iModifier);
 
-	int iBonus = GetYieldPerAlly(eIndex) * GET_PLAYER(getOwner()).GetNumCSAllies() * 100;
+	int iBonus = GetYieldPerAllyTimes100(eIndex) * GET_PLAYER(getOwner()).GetNumCSAllies();
 	return iValue + iBonus;
 }
 //	--------------------------------------------------------------------------------
@@ -25505,7 +25505,7 @@ int CvCity::GetEffectiveYieldRateFromCSFriendshipTimes100(YieldTypes eIndex) con
 
 	int iValue = GetBaseYieldRateFromCSFriendship(eIndex) * (100 + iModifier);
 
-	int iBonus = GetYieldPerFriend(eIndex) * GET_PLAYER(getOwner()).GetNumCSFriends() * 100;
+	int iBonus = GetYieldPerFriendTimes100(eIndex) * GET_PLAYER(getOwner()).GetNumCSFriends();
 	return iValue + iBonus;
 
 }
@@ -31912,8 +31912,8 @@ void CvCity::Serialize(City& city, Visitor& visitor)
 	visitor(city.m_aiYieldFromUnitLevelUp);
 	visitor(city.m_aiYieldFromUnitLevelUpGlobal);
 	visitor(city.m_aiYieldFromCombatExperienceTimes100);
-	visitor(city.m_aiYieldPerAlly);
-	visitor(city.m_aiYieldPerFriend);
+	visitor(city.m_aiYieldPerAllyTimes100);
+	visitor(city.m_aiYieldPerFriendTimes100);
 	visitor(city.m_aiYieldFromInternalTREnd);
 	visitor(city.m_aiYieldFromInternationalTREnd);
 	visitor(city.m_aiYieldFromInternalTR);

(1) Community Patch/Core Files/Overrides/Includes/InfoTooltipInclude.lua
@@ -1488,7 +1488,7 @@ function GetHelpTextForBuilding(eBuilding, bExcludeName, _, bNoMaintenance, pCit
 		end
 	end
 
-	--- Given a database table with BuildingType, <X>Type, YieldType, Yield and NumRequired, extract all entries of the given building/yield pair.
+	--- Given a database table with BuildingType, YieldType, Yield and NumRequired, extract all entries of the given building/yield pair.
 	--- Duplicate rows are overwritten.
 	--- @param tYieldFractionTable table<integer, table<string, integer>?> The yield table, could be empty or partially filled. Each entry is a table with Numerator and Denominator keys.
 	--- @param strDatabaseTable string The name of the database table to be extracted
@@ -3590,6 +3590,14 @@ function GetHelpTextForBuilding(eBuilding, bExcludeName, _, bNoMaintenance, pCit
 				tReligionBoosts[eYield] = row.Yield / 100;
 			end
 
+			for row in GameInfo.Building_YieldPerFriendTimes100{BuildingType = kBuildingInfo.Type, YieldType = kYieldInfo.Type} do
+				tCSFriendBoosts[eYield] = row.Yield / 100;
+			end
+			
+			for row in GameInfo.Building_YieldPerAllyTimes100{BuildingType = kBuildingInfo.Type, YieldType = kYieldInfo.Type} do
+				tCSAllyBoosts[eYield] = row.Yield / 100;
+			end
+			
 			local iGAYield = 0;
 			local iGAYieldCap = 0;
 			for row in GameInfo.Building_YieldChangesPerGoldenAge{BuildingType = kBuildingInfo.Type, YieldType = kYieldInfo.Type} do
@@ -3625,8 +3633,6 @@ function GetHelpTextForBuilding(eBuilding, bExcludeName, _, bNoMaintenance, pCit
 			ExtractSimpleYieldTable(tThemeBoosts, "Building_YieldChangesPerLocalTheme", kYieldInfo);
 			ExtractSimpleYieldTable(tMonopolyBoosts, "Building_YieldChangesPerMonopoly", kYieldInfo);
 			ExtractSimpleYieldTable(tFranchiseBoosts, "Building_YieldPerFranchise", kYieldInfo);
-			ExtractSimpleYieldTable(tCSFriendBoosts, "Building_YieldPerFriend", kYieldInfo);
-			ExtractSimpleYieldTable(tCSAllyBoosts, "Building_YieldPerAlly", kYieldInfo);
 		end
 
 		-- Special case for the TechEnhancedTourism column

(2) Vox Populi/Database Changes/City/Buildings/BuildingChanges.sql
@@ -1242,10 +1242,10 @@ SELECT
 FROM Buildings
 WHERE BuildingClass = 'BUILDINGCLASS_CHANCERY';
 
-INSERT INTO Building_YieldPerAlly
+INSERT INTO Building_YieldPerAllyTimes100
 	(BuildingType, YieldType, Yield)
 SELECT
-	Type, 'YIELD_PRODUCTION', 2
+	Type, 'YIELD_PRODUCTION', 200
 FROM Buildings
 WHERE BuildingClass = 'BUILDINGCLASS_CHANCERY';
 
@@ -1257,10 +1257,10 @@ SELECT
 FROM Buildings
 WHERE BuildingClass = 'BUILDINGCLASS_WIRE_SERVICE';
 
-INSERT INTO Building_YieldPerFriend
+INSERT INTO Building_YieldPerFriendTimes100
 	(BuildingType, YieldType, Yield)
 SELECT
-	Type, 'YIELD_CULTURE', 1
+	Type, 'YIELD_CULTURE', 100
 FROM Buildings
 WHERE BuildingClass = 'BUILDINGCLASS_WIRE_SERVICE';
 
@@ -1270,10 +1270,10 @@ VALUES
 	('YIELD_SCIENCE'),
 	('YIELD_CULTURE');
 
-INSERT INTO Building_YieldPerAlly
+INSERT INTO Building_YieldPerAllyTimes100
 	(BuildingType, YieldType, Yield)
 SELECT
-	a.Type, b.YieldType, 1
+	a.Type, b.YieldType, 100
 FROM Buildings a, Helper b
 WHERE a.BuildingClass = 'BUILDINGCLASS_WIRE_SERVICE';
 
@@ -1921,10 +1921,10 @@ SELECT
 FROM Buildings
 WHERE BuildingClass = 'BUILDINGCLASS_SCRIVENERS_OFFICE';
 
-INSERT INTO Building_YieldPerFriend
+INSERT INTO Building_YieldPerFriendTimes100
 	(BuildingType, YieldType, Yield)
 SELECT
-	Type, 'YIELD_FOOD', 1
+	Type, 'YIELD_FOOD', 100
 FROM Buildings
 WHERE BuildingClass = 'BUILDINGCLASS_SCRIVENERS_OFFICE';
 
@@ -1934,10 +1934,10 @@ VALUES
 	('YIELD_FOOD'),
 	('YIELD_FAITH');
 
-INSERT INTO Building_YieldPerAlly
+INSERT INTO Building_YieldPerAllyTimes100
 	(BuildingType, YieldType, Yield)
 SELECT
-	a.Type, b.YieldType, 1
+	a.Type, b.YieldType, 100
 FROM Buildings a, Helper b
 WHERE a.BuildingClass = 'BUILDINGCLASS_SCRIVENERS_OFFICE';
 

CvGameCoreDLL_Expansion2/CvBuildingClasses.h
@@ -632,8 +632,8 @@ public:
 	int GetNumFreeSpecialUnits(int i) const;
 	std::map<int, int> GetResourcePlotsToPlace(int i) const;
 	bool IsResourcePlotsToPlace() const;
-	int GetYieldPerFriend(int i) const;
-	int GetYieldPerAlly(int i) const;
+	int GetYieldPerFriendTimes100(int i) const;
+	int GetYieldPerAllyTimes100(int i) const;
 	int GetYieldChangeWorldWonder(int i) const;
 	int GetYieldChangeWorldWonderGlobal(int i) const;
 
@@ -1095,8 +1095,8 @@ private:
 	bool* m_pbBuildingClassNeededNowhere;
 	int* m_piNumSpecFreeUnits;
 	std::map<int, std::map<int, int>> m_ppiResourcePlotsToPlace;
-	int* m_piYieldPerFriend;
-	int* m_piYieldPerAlly;
+	int* m_piYieldPerFriendTimes100;
+	int* m_piYieldPerAllyTimes100;
 	int* m_piYieldChangeWorldWonder;
 	int* m_piYieldChangeWorldWonderGlobal;
 	int* m_piLuxuryYieldChanges;

CvGameCoreDLL_Expansion2/CvCity.h
@@ -1165,11 +1165,11 @@ public:
 	int GetYieldFromCombatExperienceTimes100(YieldTypes eIndex) const;
 	void ChangeYieldFromCombatExperienceTimes100(YieldTypes eIndex, int iChange);
 
-	int GetYieldPerAlly(YieldTypes eIndex) const;
-	void ChangeYieldPerAlly(YieldTypes eIndex, int iChange);
+	int GetYieldPerAllyTimes100(YieldTypes eIndex) const;
+	void ChangeYieldPerAllyTimes100(YieldTypes eIndex, int iChange);
 
-	int GetYieldPerFriend(YieldTypes eIndex) const;
-	void ChangeYieldPerFriend(YieldTypes eIndex, int iChange);
+	int GetYieldPerFriendTimes100(YieldTypes eIndex) const;
+	void ChangeYieldPerFriendTimes100(YieldTypes eIndex, int iChange);
 
 	int GetBuildingYieldFromYield(YieldTypes eIndex1, YieldTypes eIndex2) const;
 	void ChangeBuildingYieldFromYield(YieldTypes eIndex, YieldTypes eIndex2, int iValue);
@@ -1994,8 +1994,8 @@ protected:
 	std::vector<int> m_aiYieldFromUnitLevelUp;
 	std::vector<int> m_aiYieldFromUnitLevelUpGlobal;
 	std::vector<int> m_aiYieldFromCombatExperienceTimes100;
-	std::vector<int> m_aiYieldPerAlly;
-	std::vector<int> m_aiYieldPerFriend;
+	std::vector<int> m_aiYieldPerAllyTimes100;
+	std::vector<int> m_aiYieldPerFriendTimes100;
 	std::vector<int> m_aiYieldFromInternalTREnd;
 	std::vector<int> m_aiYieldFromInternationalTREnd;
 	std::vector<int> m_aiYieldFromInternalTR;
@@ -2400,8 +2400,8 @@ SYNC_ARCHIVE_VAR(std::vector<int>, m_aiYieldFromFaithPurchase)
 SYNC_ARCHIVE_VAR(std::vector<int>, m_aiYieldFromUnitLevelUp)
 SYNC_ARCHIVE_VAR(std::vector<int>, m_aiYieldFromUnitLevelUpGlobal)
 SYNC_ARCHIVE_VAR(std::vector<int>, m_aiYieldFromCombatExperienceTimes100)
-SYNC_ARCHIVE_VAR(std::vector<int>, m_aiYieldPerAlly)
-SYNC_ARCHIVE_VAR(std::vector<int>, m_aiYieldPerFriend)
+SYNC_ARCHIVE_VAR(std::vector<int>, m_aiYieldPerAllyTimes100)
+SYNC_ARCHIVE_VAR(std::vector<int>, m_aiYieldPerFriendTimes100)
 SYNC_ARCHIVE_VAR(std::vector<int>, m_aiYieldFromInternalTREnd)
 SYNC_ARCHIVE_VAR(std::vector<int>, m_aiYieldFromInternationalTREnd)
 SYNC_ARCHIVE_VAR(std::vector<int>, m_aiYieldFromInternalTR)

CvGameCoreDLL_Expansion2/CvCityStrategyAI.cpp
@@ -3788,13 +3788,13 @@ int CityStrategyAIHelpers::GetBuildingYieldValue(CvCity *pCity, BuildingTypes eB
 			}
 		}
 	}
-	if (pkBuildingInfo->GetYieldPerAlly(eYield) > 0)
+	if (pkBuildingInfo->GetYieldPerAllyTimes100(eYield) > 0)
 	{
-		iFlatYield += (pkBuildingInfo->GetYieldPerAlly(eYield) * max(GC.getGame().GetNumMinorCivsAlive() / 4, kPlayer.GetNumCSAllies()));
+		iFlatYield += (pkBuildingInfo->GetYieldPerAllyTimes100(eYield) * max(GC.getGame().GetNumMinorCivsAlive() / 4, kPlayer.GetNumCSAllies()) / 100);
 	}
-	if (pkBuildingInfo->GetYieldPerFriend(eYield) > 0)
+	if (pkBuildingInfo->GetYieldPerFriendTimes100(eYield) > 0)
 	{
-		iFlatYield += (pkBuildingInfo->GetYieldPerFriend(eYield) * max(GC.getGame().GetNumMinorCivsAlive() / 4, kPlayer.GetNumCSFriends()));
+		iFlatYield += (pkBuildingInfo->GetYieldPerFriendTimes100(eYield) * max(GC.getGame().GetNumMinorCivsAlive() / 4, kPlayer.GetNumCSFriends()) / 100);
 	}
 	if (pkBuildingInfo->GetYieldFromInternal(eYield) > 0)
 	{

CvGameCoreDLL_Expansion2/Lua/CvLuaCity.cpp
@@ -1634,8 +1634,8 @@ int CvLuaCity::lGetBuildingYieldRateTimes100(lua_State* L)
 	iYieldTimes100 += pCity->plot()->IsTradeUnitRoute() ? pkBuildingInfo->GetYieldChangeFromPassingTR(eYield) * 100 : 0;
 	iYieldTimes100 += (pkBuildingInfo->GetYieldChangePerCityStateStrategicResource(eYield) * kPlayer.GetNumStrategicResourcesFromMinors() * 100).Truncate();
 	iYieldTimes100 += pkBuildingInfo->GetYieldChangeEraScalingTimes100(eYield) * max(1, static_cast<int>(kPlayer.GetCurrentEra()));
-	iYieldTimes100 += pkBuildingInfo->GetYieldPerFriend(eYield) * kPlayer.GetNumCSFriends() * 100;
-	iYieldTimes100 += pkBuildingInfo->GetYieldPerAlly(eYield) * kPlayer.GetNumCSAllies() * 100;
+	iYieldTimes100 += pkBuildingInfo->GetYieldPerFriendTimes100(eYield) * kPlayer.GetNumCSFriends();
+	iYieldTimes100 += pkBuildingInfo->GetYieldPerAllyTimes100(eYield) * kPlayer.GetNumCSAllies();
 	iYieldTimes100 += pkBuildingInfo->GetYieldChangePerMonopoly(eYield) * kPlayer.GetNumGlobalMonopolies() * 100;
 	iYieldTimes100 += (pkBuildingInfo->GetYieldChangePerBuilding(eYield) * pCity->GetCityBuildings()->GetNumBuildings() * 100).Truncate();
 

commit 94acdbb1d849265f4216e06bc25cdb0acfb35500

Fix negative CS ally/friend yield underflow in city building processing
Resolve an assertion in CvCity::ChangeYieldPerAllyTimes100 triggered during city capture/PreKill building removal flows.

Root-cause history:
- Issue introduced by commit e7680355b ("CS Friend and Ally tables now x100 for future balancing (#12442)").
- That change wired per-building ally/friend deltas into CvCity::processBuilding, making null/legacy underflow paths user-visible in capture removal flows.

Changes:
- CvBuildingEntry: return 0 (not -1) for missing YieldPerAllyTimes100/YieldPerFriendTimes100 arrays.
- CvCity: clamp ChangeYieldPerAllyTimes100 and ChangeYieldPerFriendTimes100 accumulators to non-negative values.
- CvCity::read: sanitize legacy negative per-ally/per-friend cached yield values from saves by resetting them to 0 after deserialize.

Impact:
- Prevents GetYieldPerAllyTimes100/GetYieldPerFriendTimes100 assertions during building add/remove mismatch and old-save edge cases.
- Keeps runtime state valid without changing intended positive-yield behavior.

CvGameCoreDLL_Expansion2/CvBuildingClasses.cpp
@@ -4592,14 +4592,14 @@ int CvBuildingEntry::GetYieldPerFriendTimes100(int i) const
 {
 	PRECONDITION(i < NUM_YIELD_TYPES, "Index out of bounds");
 	PRECONDITION(i > -1, "Index out of bounds");
-	return m_piYieldPerFriendTimes100 ? m_piYieldPerFriendTimes100[i] : -1;
+	return m_piYieldPerFriendTimes100 ? m_piYieldPerFriendTimes100[i] : 0;
 }
 
 int CvBuildingEntry::GetYieldPerAllyTimes100(int i) const
 {
 	PRECONDITION(i < NUM_YIELD_TYPES, "Index out of bounds");
 	PRECONDITION(i > -1, "Index out of bounds");
-	return m_piYieldPerAllyTimes100 ? m_piYieldPerAllyTimes100[i] : -1;
+	return m_piYieldPerAllyTimes100 ? m_piYieldPerAllyTimes100[i] : 0;
 }
 
 int CvBuildingEntry::GetYieldChangeWorldWonder(int i) const

CvGameCoreDLL_Expansion2/CvCity.cpp
@@ -24765,6 +24765,10 @@ void CvCity::ChangeYieldPerAllyTimes100(YieldTypes eIndex, int iChange)
 	if (iChange != 0)
 	{
 		m_aiYieldPerAllyTimes100[eIndex] = m_aiYieldPerAllyTimes100[eIndex] + iChange;
+		if (m_aiYieldPerAllyTimes100[eIndex] < 0)
+		{
+			m_aiYieldPerAllyTimes100[eIndex] = 0;
+		}
 		ASSERT(GetYieldPerAllyTimes100(eIndex) >= 0);
 	}
 }
@@ -24789,6 +24793,10 @@ void CvCity::ChangeYieldPerFriendTimes100(YieldTypes eIndex, int iChange)
 	if (iChange != 0)
 	{
 		m_aiYieldPerFriendTimes100[eIndex] = m_aiYieldPerFriendTimes100[eIndex] + iChange;
+		if (m_aiYieldPerFriendTimes100[eIndex] < 0)
+		{
+			m_aiYieldPerFriendTimes100[eIndex] = 0;
+		}
 		ASSERT(GetYieldPerFriendTimes100(eIndex) >= 0);
 	}
 }
@@ -32503,6 +32511,18 @@ void CvCity::read(FDataStream& kStream)
 	// Perform shared serialize
 	CvStreamLoadVisitor serialVisitor(kStream);
 	Serialize(*this, serialVisitor);
+
+	for (int i = 0; i < NUM_YIELD_TYPES; i++)
+	{
+		if (m_aiYieldPerAllyTimes100[i] < 0)
+		{
+			m_aiYieldPerAllyTimes100[i] = 0;
+		}
+		if (m_aiYieldPerFriendTimes100[i] < 0)
+		{
+			m_aiYieldPerFriendTimes100[i] = 0;
+		}
+	}
 
 	GetCityStrategyAI()->PrecalcYieldStats();
 